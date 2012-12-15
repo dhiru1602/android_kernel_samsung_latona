8fca4b34ce4ae533ccc181200c43268ac0f31fc6
Dheeraj CVR <cvr.dheeraj@gmail.com>
2012-12-15 16:07:34 +0530

    mach: opp: disable l3_main on latona
    Breaks DVFS

--- arch/arm/mach-omap2/opp3xxx_data.c
+++ arch/arm/mach-omap2/opp3xxx_data.c
@@ -159,8 +159,13 @@ static struct omap_opp_def __initdata omap36xx_opp_def_list[] = {
 				1000000000, OMAP3630_VDD_MPU_OPP1G_UV),
 
 	/* L3 OPP1 - OPP50 */
+#ifdef CONFIG_MACH_OMAP_LATONA
+	OPP_INITIALIZER("l3_main", "l3_ick", "core", false,
+				100000000, OMAP3630_VDD_CORE_OPP50_UV),
+#else
 	OPP_INITIALIZER("l3_main", "l3_ick", "core", true,
 				100000000, OMAP3630_VDD_CORE_OPP50_UV),
+#endif
 	/* L3 OPP2 - OPP100, OPP-Turbo, OPP-SB */
 	OPP_INITIALIZER("l3_main", "l3_ick", "core", true,
 				200000000, OMAP3630_VDD_CORE_OPP100_UV),
