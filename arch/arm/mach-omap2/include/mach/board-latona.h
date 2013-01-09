58b17633cfde8cd97ce4a7da103ef069d46088f4
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-01-09 18:21:43 +0530

    mach:latona: cleanup latona board header file and makefile

--- arch/arm/mach-omap2/include/mach/board-latona.h
+++ arch/arm/mach-omap2/include/mach/board-latona.h
@@ -119,27 +119,3 @@ extern void (*latona_get_param_value) (int idx, void *value);
 #define USB_SEL_MASK			(1 << 0)
 #define UART_SEL_MASK			(1 << 1)
 /* END: Default Parameter Values */
-
-#if (defined(CONFIG_VIDEO_IMX046) || defined(CONFIG_VIDEO_IMX046_MODULE)) && \
-	defined(CONFIG_VIDEO_OMAP3)
-#include <media/imx046.h>
-extern struct imx046_platform_data latona_imx046_platform_data;
-#endif
-
-#ifdef CONFIG_VIDEO_OMAP3
-extern void latona_cam_init(void);
-#else
-#define latona_cam_init()	NULL
-#endif
-
-#if (defined(CONFIG_VIDEO_LV8093) || defined(CONFIG_VIDEO_LV8093_MODULE)) && \
-	defined(CONFIG_VIDEO_OMAP3)
-#include <media/lv8093.h>
-extern struct lv8093_platform_data latona_lv8093_platform_data;
-#define LV8093_PS_GPIO		7
-/* GPIO7 is connected to lens PS pin through inverter */
-#define LV8093_PWR_OFF		1
-#define LV8093_PWR_ON		(!LV8093_PWR_OFF)
-#endif
-
-#define LATONA_HEADSET_EXTMUTE_GPIO	153
