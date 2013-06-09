04882bf1584c6910781740494a05fc1b80a36f3e
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-06-09 20:21:03 +0530

    drivers: musb: hold and release a wakelock only in host mode

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -728,7 +728,8 @@ b_host:
 		handled = IRQ_HANDLED;
 
 		/* Release the wakelock */
-		wake_unlock(&musb->musb_wakelock);
+		if (is_otg_enabled(musb) || is_host_enabled(musb))
+			wake_unlock(&musb->musb_wakelock);
 		switch (musb->xceiv->state) {
 #ifdef CONFIG_USB_MUSB_HDRC_HCD
 		case OTG_STATE_A_HOST:
@@ -797,7 +798,8 @@ b_host:
 				otg_state_string(musb->xceiv->state));
 
 			/* Hold a wakelock */
-			wake_lock(&musb->musb_wakelock);
+			if (is_otg_enabled(musb) || is_host_enabled(musb))
+				wake_lock(&musb->musb_wakelock);
 			switch (musb->xceiv->state) {
 #ifdef CONFIG_USB_OTG
 			case OTG_STATE_A_SUSPEND:
