b4f0e67015ad58211bef60474647b472805635aa
sconosciuto <sconosciuto.git@gmail.com>
2013-08-13 18:52:54 +0200

    drivers: leds: latona: code cleanup and fixes
     * Let LEDs be turned off from the userspace
     * Turn both the LEDs on when softkeys are touched
     * Keep LEDs on while touching

--- drivers/leds/leds-latona.c
+++ drivers/leds/leds-latona.c
@@ -29,8 +29,8 @@ struct latona_led_data {
 	u8 can_sleep2;
 };
 
-static int led_state =0;
-static int bln_state=0;
+static int bln_state = 0;
+static int pressing = 0;
 
 static int bl_timeout = 1600;
 static struct timer_list bl_timer;
@@ -41,54 +41,56 @@ void bl_timer_callback(unsigned long data)
 {
 	schedule_work(&bl_off_work);
 }
+
 static void bl_off(struct work_struct *bl_off_work)
 {
-	if(bln_state==0)
-	{
+	if (bln_state == 0 && pressing == 0) {
 		gpio_set_value(OMAP_GPIO_LED_EN1, 0);
 		gpio_set_value(OMAP_GPIO_LED_EN2, 0);
 	}
-	led_state = 0;
 }
 
-void bl_set_timeout() {
+static void bl_set_timeout(void)
+{
 	if (bl_timeout > 0) {
 		mod_timer(&bl_timer, jiffies + msecs_to_jiffies(bl_timeout));
 	}
 }
 
-void trigger_touchkey_led(int event)
+static void trigger_touchkey_led(int event)
 {
-	//event: 0-All Lights | 1-Menu Pressed | 2-Back Pressed	| 3- All Key Release
-	if(bl_timeout!=0)
-	{
-		if((event==3)&&(led_state==0)) return; //Dont lightup if keys already turned off
-
-		if((event==0)||(event==3))
-		{
+	if (bl_timeout != 0) {
+		pressing = 0;
+		switch (event) {
+		case TOUCHKEY_ALL:
 			gpio_set_value(OMAP_GPIO_LED_EN1, 1);
 			gpio_set_value(OMAP_GPIO_LED_EN2, 1);
-		}else if(event==1){
-			gpio_set_value(OMAP_GPIO_LED_EN1, 0);
-			gpio_set_value(OMAP_GPIO_LED_EN2, 1);
-		}else if(event==2){
+			bl_set_timeout();
+			break;
+		case TOUCHKEY_MENU:
+		case TOUCHKEY_BACK:
+			pressing = 1;
 			gpio_set_value(OMAP_GPIO_LED_EN1, 1);
-			gpio_set_value(OMAP_GPIO_LED_EN2, 0);
+			gpio_set_value(OMAP_GPIO_LED_EN2, 1);
+			break;
+		case TOUCHKEY_NONE:
+			bl_set_timeout();
+			break;
+		default:
+			pr_err("%s: unknown event: %d\n", __func__, event);
+			break;
 		}
-		led_state = 1;
-		bln_state = 0;
-		bl_set_timeout();
 	}
 }
 
 void suspend_touchkey_led(void)
 {
-	if(bln_state==0)
-	{
+	cancel_work_sync(&bl_off_work);
+	del_timer(&bl_timer);
+	if (bln_state == 0) {
 		gpio_set_value(OMAP_GPIO_LED_EN1, 0);
 		gpio_set_value(OMAP_GPIO_LED_EN2, 0);
 	}
-	led_state = 0;
 }
 
 void latona_leds_report_event(int key_code, int value)
@@ -117,31 +119,20 @@ static void latona_led_set(struct led_classdev *led_cdev,
 {
 	struct latona_led_data *led_dat =
 		container_of(led_cdev, struct latona_led_data, cdev);
-    
-	if ((value == LED_OFF)&&( bln_state==1))
-	{
-		if (led_state && bl_timer.expires < jiffies)
-		{
-			gpio_set_value(led_dat->gpio1, 0);
-			gpio_set_value(led_dat->gpio2, 0);
-			printk(KERN_DEBUG "[LED] %s ::: OFF \n", __func__);
-		}
+
+	if (value == LED_OFF) {
+		gpio_set_value(led_dat->gpio1, 0);
+		gpio_set_value(led_dat->gpio2, 0);
 		bln_state = 0;
-	}
-	else if ((value != LED_OFF)&&(bln_state==0))
-	{
+	} else {
 		gpio_set_value(led_dat->gpio1, 1);
 		gpio_set_value(led_dat->gpio2, 1);
 		bln_state = 1;
-		printk(KERN_DEBUG "[LED] %s ::: ON \n", __func__);
-	}
-	else
-	{ 
-		return ;
 	}
 }
 
-static ssize_t bl_timeout_read(struct device *dev, struct device_attribute *attr, char *buf) {
+static ssize_t bl_timeout_read(struct device *dev, struct device_attribute *attr, char *buf)
+{
 	return sprintf(buf,"%d\n", bl_timeout);
 }
 
@@ -154,29 +145,28 @@ static ssize_t bl_timeout_write(struct device *dev, struct device_attribute *att
 static DEVICE_ATTR(bl_timeout, S_IRUGO | S_IWUGO, bl_timeout_read, bl_timeout_write);
 
 static struct attribute *bl_led_attributes[] = {
-&dev_attr_bl_timeout.attr,
-NULL
+	&dev_attr_bl_timeout.attr,
+	NULL
 };
 
 static struct attribute_group bl_led_group = {
-.attrs = bl_led_attributes,
+	.attrs = bl_led_attributes,
 };
 
 static struct miscdevice bl_led_device = {
-.minor = MISC_DYNAMIC_MINOR,
-.name = "notification",
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "notification",
 };
 
 static int latona_led_probe(struct platform_device *pdev)
-{	
-
+{
 	struct led_platform_data *pdata = pdev->dev.platform_data;
 	struct led_info *cur_led;
 	struct latona_led_data *leds_data, *led_dat;
 	
 	int i, ret = 0;
 
-	printk("[LED] %s +\n", __func__);
+	pr_debug("%s\n", __func__);
 
 	if (!pdata)
 		return -EBUSY;
@@ -212,10 +202,10 @@ static int latona_led_probe(struct platform_device *pdev)
 	setup_timer(&bl_timer, bl_timer_callback, 0);
 
 	if (misc_register(&bl_led_device))
-		printk("%s misc_register(%s) failed\n", __FUNCTION__, bl_led_device.name);
+		pr_err("%s: misc_register(%s) failed\n", __FUNCTION__, bl_led_device.name);
 	else {
 		if (sysfs_create_group(&bl_led_device.this_device->kobj, &bl_led_group) < 0)
-			pr_err("failed to create sysfs group for device %s\n", bl_led_device.name);
+			pr_err("%s: failed to create sysfs group for device %s\n", __func__, bl_led_device.name);
 	}
 
 	return 0;
@@ -249,13 +239,11 @@ static int __devexit latona_led_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static int latona_led_shutdown(struct platform_device *pdev)
+static void latona_led_shutdown(struct platform_device *pdev)
 {
 	gpio_direction_output(OMAP_GPIO_LED_EN1, 0);
 	gpio_direction_output(OMAP_GPIO_LED_EN2, 0);
 	del_timer(&bl_timer);
-
-	return 0;
 }
 
 static struct platform_driver latona_led_driver = {
@@ -270,7 +258,7 @@ static struct platform_driver latona_led_driver = {
 
 static int __init latona_led_init(void)
 {
-	printk("[LED] %s +\n", __func__);
+	pr_debug("%s", __func__);
 	return platform_driver_register(&latona_led_driver);
 }
 
