a2d067615e90e210663f669fda0c70b296069e16
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-01-21 04:43:30 +0530

    mach: latona-power: use GPIO again to monitor full charge
    It seems that the inaccurate SOC and VC values are causing
    false reporting of Full battery state, which prevents proper
    full charging of the device. CHG_EN GPIO was being used in
    the 2.6 Samsung Kernel too. Assuming that the max8855 charger
    would prevent overcharging, we could safely bring back the
    GPIO check.
    
    Also adjusted the full charge VC to match the 2.6 kernel.

--- arch/arm/mach-omap2/board-latona-power.c
+++ arch/arm/mach-omap2/board-latona-power.c
@@ -160,13 +160,8 @@ static int temp_adc_value(void)
 
 static bool check_charge_full(void)
 {
-	/*
-	 * The GPIO is high when the battery is fully charged, but
-	 * we want to stop before that happens.
-	 */
 	// HIGH : BATT_FULL || LOW : CHARGING / DISCHARGING
-	//return gpio_get_value(charger_gpios[GPIO_CHG_ING_N].gpio);
-	return true;
+	return gpio_get_value(charger_gpios[GPIO_CHG_ING_N].gpio);
 }
 
 static int get_bat_temp_by_adc(int *batt_temp)
@@ -284,8 +279,8 @@ static struct max17040_platform_data max17040_pdata = {
 	.high_recover_temp = HIGH_RECOVER_TEMP_LATONA,
 	.low_block_temp = LOW_BLOCK_TEMP_LATONA,
 	.low_recover_temp = LOW_RECOVER_TEMP_LATONA,
-	.fully_charged_vol = 4150000,
-	.recharge_vol = 4140000,
+	.fully_charged_vol = 4050000,
+	.recharge_vol = 4040000,
 	.limit_charging_time = 21600,  /* 6 hours */
 	.limit_recharging_time = 5400, /* 90 min */
 };
