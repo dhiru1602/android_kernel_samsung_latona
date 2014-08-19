7b8d73dd32e6cd21107f9a963a32a5ed237a6e78
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-08-20 03:55:13 +0530

    Revert "latona: wifi: set enabled_at_boot flag to true"
    GPIO_WLAN_EN state depends on the bootloader and it appears that
    certain bootloader versions are toggling them high on boot whereas
    others are leaving them low. Few users have reported issues with
    no wifi after boot probably because they have a bootloader that
    behaves differently.
    
    This reverts commit fad461edb13ec9ba10031960dab0deddb3905387.
    
    Change-Id: I18b0debdb6139a5e768ad0707cc9ca604ec22aa8

--- arch/arm/mach-omap2/board-latona-peripherals.c
+++ arch/arm/mach-omap2/board-latona-peripherals.c
@@ -341,7 +341,7 @@ static struct fixed_voltage_config latona_vwlan = {
 	.gpio			= LATONA_WIFI_PMENA_GPIO,
 	.startup_delay		= 70000, /* 70msec */
 	.enable_high		= 1,
-	.enabled_at_boot	= 1,
+	.enabled_at_boot	= 0,
 	.init_data		= &latona_vmmc3,
 };
 
